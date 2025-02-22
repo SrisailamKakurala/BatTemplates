b684fd67a04551c9f3023f5cdd27c2d92b2100e0
srisailamkakurala <srisailamkakurala@gmail.com>
2025-02-22 18:45:45 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  server: {
-    host: '0.0.0.0', // Listen on all network interfaces
-    port: 5173, // Default port
-  },
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_'); // Load env variables
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+    },
+    define: {
+      'import.meta.env': env, // Ensure env variables are accessible
+    },
+  };
+});
